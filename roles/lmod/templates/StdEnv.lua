f858dbb4490e106fabfee34f23aa000ddf9eda97
Kiril Tsyganov <kirill.tsyganov@monash.edu>
2016-01-18 13:27:35 +1100

    milestone 2

---
+++ roles/lmod/templates/StdEnv.lua
@@ -0,0 +1,25 @@
+load("samtools"
+    ,"vcftools"
+    ,"bcftools"
+
+    ,"tophat"
+    ,"cufflinks"
+
+    ,"picard"
+    ,"blast"
+    ,"blat"
+    ,"BWA"
+    ,"STAR"
+    ,"SPAdes"
+
+    ,"igv"
+    ,"igvtools"
+    ,"gatk"
+    ,"bedops"
+
+    ,"bowtie2"
+
+    ,"parallel"
+
+    ,"java"
+    )
